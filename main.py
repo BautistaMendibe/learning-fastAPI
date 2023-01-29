71de6aa4a382b16a5a2a465281a23b4ce927ad24
BautistaMendibe <bautistamendibe10@gmail.com>
2023-01-29 18:07:04 -0300

    API for users

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return "Hola!"
+
+
+@app.get("/url")
+async def url():
+    return {
+        "url" : "http://wwww.localhost.com"
+    }
---
+++ users.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# uvicorn users:app --reload
+
+@app.get("/users")
+async def users():
+    return {
+        {
+            "name": "Bautista",
+            "apellido": "Mendibe",
+            "mail": "bautistamendibe@gmail.com"
+        },
+        {
+            "name": "Juan",
+            "apellido": "Mendibe",
+            "mail": "bautistamendibe@gmail.com"
+        },
+        {
+            "name": "Pedro",
+            "apellido": "Mendibe",
+            "mail": "bautistamendibe@gmail.com"
+        }   
+    }
