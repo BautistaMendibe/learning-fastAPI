dca91fc6b7f04f72cf0a14c0eb813f42f587bdde
BautistaMendibe <bautistamendibe10@gmail.com>
2023-01-29 18:19:08 -0300

    Clase user

--- users.py
+++ users.py
@@ -4,22 +4,32 @@ app = FastAPI()
 
 # uvicorn users:app --reload
 
+# Entidad user
+class User():
+    name: str
+    apellido: str
+    mail: str
+    ege: int
+
 @app.get("/users")
 async def users():
     return [
         {
             "name": "Bautista",
             "apellido": "Mendibe",
-            "mail": "bautistamendibe@gmail.com"
+            "mail": "bautistamendibe@gmail.com",
+            "ege": 22
         },
         {
             "name": "Juan",
             "apellido": "Mendibe",
-            "mail": "bautistamendibe@gmail.com"
+            "mail": "bautistamendibe@gmail.com",
+            "ege": 25
         },
         {
             "name": "Pedro",
             "apellido": "Mendibe",
-            "mail": "bautistamendibe@gmail.com"
+            "mail": "bautistamendibe@gmail.com",
+            "ege": 40
         }   
     ]
